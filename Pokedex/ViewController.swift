ac60f14454da3cb1283de5c2d1a3dcbbf75448ab
Heesu Yun <Yunheesu@icloud.com>
2020-04-06 16:37:43 +0900

    Pokedex Exercise #1

--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView! // tableView연결!
+    // remember! dataSource + delegate!
+    
+    var pokeArray = ["Bulbasaur", "Pikachu", "Snorlax", "Wigglytuff", "Charmander"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
 
 }
-
+extension ViewController: UITableViewDelegate, UITableViewDataSource { // putting tableview calls into extension
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pokeArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //remember arrays + indePaths are zero-indexed, so you need indexPath.row + 1 to start counting at 1 instead of 0 .
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row + 1). \(pokeArray[indexPath.row])" // indexPath.row = 숫자!
+        return cell
+    }
+    
+    
+}
