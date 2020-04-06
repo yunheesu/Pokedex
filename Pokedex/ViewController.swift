4704543351f7bd242ef0602178ec652f222f3f1e
Heesu Yun <Yunheesu@icloud.com>
2020-04-06 15:56:45 +0900

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Heesu Yun on 4/6/20.
+//  Copyright © 2020 Heesu Yun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
